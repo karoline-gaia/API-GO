e8d1851042c710d2ed23eb9b549e49dd79ad2ff5
Karoline Gaia <karol.gaia.alex@gmail.com>
2025-02-01 16:41:39 -0300

    feat - Create .env

--- configs/config.go
+++ configs/config.go
@@ -1,5 +1,10 @@
 package configs
 
+import (
+	"github.com/go-chi/jwtauth"
+	"github.com/spf13/viper"
+)
+
 var cfg *conf
 
 type conf struct {
@@ -12,6 +17,21 @@ type conf struct {
 	WebServerPort string
 	JWTScecret    string
 	JwtExperesIn  int
+	TokenAuth     *jwtauth.JWTAuth
 }
 
-func LoadConfig(path string) (*conf, error)
+func LoadConfig(path string) (*conf, error) {
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		panic(err)
+	}
+}
