f627973ee35af8457e9bdeee7da47b805624e14a
Karoline Gaia Alexandre <karol.gaia.alex@gmail.com>
2025-03-10 16:57:59 -0300

    feat/products tests

--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -6,3 +6,11 @@ type UserInterface interface {
 	CreateUser(user *entity.User) error
 	FindByEmail(email string) (*entity.User, error)
 }
+
+type PoductInterface interface {
+	CreateProduct(product *entity.Product) error
+	FindAll(page, limt int, sort string) ([]entity.Product, error)
+	FindById(id string) (*entity.Product, error)
+	UpdateProduct(product *entity.Product) error
+	DeleteProduct(id string) error
+}
